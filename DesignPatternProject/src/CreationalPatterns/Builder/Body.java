fd0f494058902972833a862371fee24f9d4b5fbe
10uranli <onur.anli@bil.omu.edu.tr>
2019-01-27 18:11:17 +0300

    Builder

---
+++ DesignPatternProject/src/CreationalPatterns/Builder/Body.java
@@ -0,0 +1,29 @@
+package CreationalPatterns.Builder;
+
+public class Body {
+
+	private int arms = 2;
+	private int ears = 2;
+	private int eyes = 2;
+	public int getArms() {
+		return arms;
+	}
+	public void setArms(int arms) {
+		this.arms = arms;
+	}
+	public int getEars() {
+		return ears;
+	}
+	public void setEars(int ears) {
+		this.ears = ears;
+	}
+	public int getEyes() {
+		return eyes;
+	}
+	public void setEyes(int eyes) {
+		this.eyes = eyes;
+	}
+	
+	
+	
+}
